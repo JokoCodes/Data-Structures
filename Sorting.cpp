11dbbfa005ffcc79d99e00af8354ffad672a2842
willi4cl <55256180+willi4cl@users.noreply.github.com>
2019-10-21 15:27:04 -0400

    Add files via upload

--- Sorting.cpp
+++ Sorting.cpp
@@ -10,14 +10,24 @@ using namespace std;
 	typedef std::chrono::high_resolution_clock Clock;
 
 	int main()	{
-	//    auto t1 = Clock::now();
-	 //   auto t2 = Clock::now();
+	    auto t1 = Clock::now();
 		Sorting x;
-		x.createArray(10);
-		x.display();
-		x.BubbleSort();
-		x.display();
-	//std::cout << "Delta t2-t1: "
- //<< std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count() 
- //<< " nanoseconds" << std::endl;
-	}
+		int Test1[10] = {};
+		//int Test2[100] = {};
+		//Sorting y;
+		x.createArray(Test1,10);
+		x.display(Test1,10);
+		//y.createArray(15);
+		//x.display();
+		//y.display();
+		//x.insertionSort(Test1,10);
+		//x.quickSort(Test1, 0, 10);
+		x.BubbleSort(Test1,10);
+		//y.BubbleSort();
+		x.display(Test1,10);
+		//y.display();
+		auto t2 = Clock::now();
+	std::cout << "Delta t2-t1: "
+ << std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count() 
+ << " nanoseconds" << std::endl;
+ 	}
--- Sorting.h
+++ Sorting.h
@@ -11,153 +11,133 @@ using namespace std;
 
 class Sorting {
 public:
-	/*
-	std::vector <int> createArray(int size) {
+	void swap(int* a, int* b) { 
+		int x = *a;
+		*a = *b;
+		*b = x;
+	}
+
+	void createArray(int x[], int length) {
 		srand(time(0));
-		x.resize(size + 1);
-		for (int i = 0; i < size; i++) {
-			x.at(i) = rand() % (2 * size) + 1;
+		for (int i = 0; i < length; i++) {
+			x[i] = rand() % (2 * length) + 1;
 		}
-		return x;
-	}
-*/
-     void display() {
-		for (int i = 0; i < x.size() - 1; i++) {
-			std::cout << x.at(i) << " ";
-		}
-		std::cout << std::endl;
 	}
 
-	bool isGood() {
+	void BubbleSort(int x[], int length) {
 
-	for(int j=0; j<x.size()-1; j++){
-        if(x.at(j)< x.at(j+1)){
-            sorted = true;
-        }
-        else{
-            sorted = false;
-        }
-	}
-
-	return sorted;
-	}
-
-	std::vector <int> BubbleSort() {
-		//while (sorted == false) {
-			int i = 0;
-			while (i < x.size() - 1) {
-				if (x.at(i) > x.at(i + 1)) {
-					std::swap(x.at(i), x.at(i + 1));
-					i++;
-				}
-				else {
-					i++;
+		for (int i = 0; i < length - 1; i++) {
+			for (int j = 0; j < length - i - 1; j++) {
+				if (x[j] > x[j + 1]) {
+					std::swap(x[j], x[j + 1]);
 				}
 			}
-		//	sorted = true;
-		//}
-			return x;
 		}
-	std::vector <int> mergeSort(){
+	}
+	/*
+	std::vector <int> mergeSort() {
 
-    int length = x.size();
-    int leftSize;
-    int rightSize;
+		int length = x.size();
+		int leftSize;
+		int rightSize;
 
-    //Determining where to split the array in half at
-    if(length % 2 == 0){
-        leftSize = length/2;
-        rightSize = length/2;
-    }
-    else{
-        leftSize = length/2 + 1;
-        rightSize = length/2;
-    }
+		//Determining where to split the array in half at
+		if (length % 2 == 0) {
+			leftSize = length / 2;
+			rightSize = length / 2;
+		}
+		else {
+			leftSize = length / 2 + 1;
+			rightSize = length / 2;
+		}
 
-    std::vector <int> arrayLeft;
-    std::vector <int> arrayRight;
+		std::vector <int> arrayLeft;
+		std::vector <int> arrayRight;
 
-    //Coping the original array into the left half
-    for(int j = 0; j < leftSize; j++){
-        arrayLeft.at(j) = x.at(j);
-    }
-    //Coping the original array into the right half
-    for(int j = 0; j < rightSize; j++){
-        arrayRight.at(j) = x.at(j);
-    }
+		//Coping the original array into the left half
+		for (int j = 0; j < leftSize; j++) {
+			arrayLeft.at(j) = x.at(j);
+		}
+		//Coping the original array into the right half
+		for (int j = 0; j < rightSize; j++) {
+			arrayRight.at(j) = x.at(j);
+		}
 
-    //Sort the Left array
-    for(int i = 0; i < leftSize; i++){
+		//Sort the Left array
+		for (int i = 0; i < leftSize; i++) {
 
-        if( arrayLeft.at(i) < arrayLeft.at(i+1)){
+			if (arrayLeft.at(i) < arrayLeft.at(i + 1)) {
 
-        }
-        else{
-            arrayLeft.at(i) = arrayLeft.at(i+1);
-        }
-    }
+			}
+			else {
+				arrayLeft.at(i) = arrayLeft.at(i + 1);
+			}
+		}
 
-    //Sort the right array
-    for(int i = 0; i < rightSize; i++){
+		//Sort the right array
+		for (int i = 0; i < rightSize; i++) {
 
-        if( arrayRight.at(i) < arrayRight.at(i+1)){
+			if (arrayRight.at(i) < arrayRight.at(i + 1)) {
 
-        }
-        else{
-            arrayRight.at(i) = arrayRight.at(i+1);
-        }
-    }
+			}
+			else {
+				arrayRight.at(i) = arrayRight.at(i + 1);
+			}
+		}
 
-    //Combining the Arrays
-    for(int i= 0; i < length-1; i++){
-        if (i <= leftSize){
-        x.at(i) = arrayLeft.at(i);
-        }
-        else{
-            x.at(i) = arrayRight.at(i);
-        }
-    }
+		//Combining the Arrays
+		for (int i = 0; i < length - 1; i++) {
+			if (i <= leftSize) {
+				x.at(i) = arrayLeft.at(i);
+			}
+			else {
+				x.at(i) = arrayRight.at(i);
+			}
+		}
 
-    return x;
-}
+		return x;
+	}
+	*/
+	void insertionSort(int x[], int length) {  //Snippit slightly modified from http://www.algolist.net/Algorithms/Sorting/Insertion_sort
+		for (int i = 1; i < length; i++) {
+			int j = i;
+			while (x[j] < x[j-1] && j > 0) {
+				std::swap(x[j], x[j - 1]);
+				j--;
+			}
+		}
+	}
 
-    std::vector<int> insertionSort(){
+	int splitArrays(int x[], int beginning, int end) { //Snippit slightly modified from https://www.geeksforgeeks.org/quick-sort/
+		int pivot = x[end - 1];
+		int i = beginning - 1; 
+		for (int j = beginning; j <= end - 1; j++) {
+			if (x[j] < pivot) {
+				i++;
+				swap(&x[i], &x[j]);
+			}
+		}
+		swap(&x[i + 1], &x[end - 1]);
+		return (i + 1);
+	};
 
+	void quickSort(int x[], int beginning, int end) { //Snippit slightly modified from https://www.geeksforgeeks.org/quick-sort/
+		if (beginning < end) {
+			int pi = splitArrays(x, beginning, end);
+			quickSort(x, beginning, pi - 1);
+			quickSort(x, pi + 1, end);
+		}
+	}
 
-    int i = 0;
-    while( !isGood() || i <= x.size()-1 ){
-    for(; i < x.size()-1; i++ ){
-
-        if(x.at(i+1) < x.at(i)){
-
-            std::swap(x.at(i+1), x.at(i));
-        }
-        else if(x.at(i) > x.at(i+1)){
-            std::swap(x.at(i), x.at(i+1));
-        }
-        else{
-            x.at(i) = x.at(i);
-        }
-    }
-
-    for (int i = 0; i < x.size() - 1; i++) {
-			std::cout<< x.at(i) << " ";
+	void display(int x[],int length) {
+		for (int i = 0; i < length; i++) {
+			cout << x[i] << " ";
 		}
 		std::cout << std::endl;
-    }
-
-    return x;
-    }
-
-
+	}
 
 private:
-	std::vector <int> x = {20, 9, 40 ,2, 7, 15, 10, 3, 18, 13};
-    bool sorted = false;
+
 };
-
-
-
-
 
 
