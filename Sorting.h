6f16362447efa52a929727d32a8125ba8689af3f
JokoCodes <55161770+JokoCodes@users.noreply.github.com>
2019-10-20 22:01:51 -0400

    tried to add merge and insertion

--- Sorting.h
+++ Sorting.h
@@ -1,9 +1,17 @@
 #pragma once
 #include <vector>
 #include <array>
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include<cstring>
+#include<cstdlib>
+
+using namespace std;
 
 class Sorting {
 public:
+	/*
 	std::vector <int> createArray(int size) {
 		srand(time(0));
 		x.resize(size + 1);
@@ -12,7 +20,7 @@ public:
 		}
 		return x;
 	}
-
+*/
 	std::vector <int> BubbleSort() {
 		//while (sorted == false) {
 			int i = 0;
@@ -29,7 +37,89 @@ public:
 		//}
 			return x;
 		}
-	
+	std::vector <int> mergeSort(){
+
+    int length = x.size();
+    int leftSize;
+    int rightSize;
+
+    //Determining where to split the array in half at
+    if(length % 2 == 0){
+        leftSize = length/2;
+        rightSize = length/2;
+    }
+    else{
+        leftSize = length/2 + 1;
+        rightSize = length/2;
+    }
+
+    std::vector <int> arrayLeft;
+    std::vector <int> arrayRight;
+
+    //Coping the original array into the left half
+    for(int j = 0; j < leftSize; j++){
+        arrayLeft.at(j) = x.at(j);
+    }
+    //Coping the original array into the right half
+    for(int j = 0; j < rightSize; j++){
+        arrayRight.at(j) = x.at(j);
+    }
+
+    //Sort the Left array
+    for(int i = 0; i < leftSize; i++){
+
+        if( arrayLeft.at(i) < arrayLeft.at(i+1)){
+
+        }
+        else{
+            arrayLeft.at(i) = arrayLeft.at(i+1);
+        }
+    }
+
+    //Sort the right array
+    for(int i = 0; i < rightSize; i++){
+
+        if( arrayRight.at(i) < arrayRight.at(i+1)){
+
+        }
+        else{
+            arrayRight.at(i) = arrayRight.at(i+1);
+        }
+    }
+
+    //Combining the Arrays
+    for(int i= 0; i < length-1; i++){
+        if (i <= leftSize){
+        x.at(i) = arrayLeft.at(i);
+        }
+        else{
+            x.at(i) = arrayRight.at(i);
+        }
+    }
+
+    return x;
+}
+
+    std::vector<int> insertionSort(){
+
+    int i = 0;
+    while(i < x.size()-1){
+    for(; i < x.size()-1; i++ ){
+
+        if(x.at(i+1) < x.at(i)){
+
+            x.at(i) = x.at(i+1);
+            i++;
+        }
+        else{
+
+        }
+    }
+    }
+
+    return x;
+    }
+
 	void display() {
 		for (int i = 0; i < x.size() - 1; i++) {
 			std::cout << x.at(i) << " ";
@@ -37,8 +127,11 @@ public:
 		std::cout << std::endl;
 	}
 
-private: 
-	std::vector <int> x;
-	
+private:
+	std::vector <int> x = {9, 20, 40 ,2, 7, 15, 10, 3, 18, 13};
+
 	bool sorted = false;
 };
+
+
+
