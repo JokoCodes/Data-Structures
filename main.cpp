f37016e94b09d664eaa1ded41d962b9248ebc261
JokoCodes <55161770+JokoCodes@users.noreply.github.com>
2019-11-13 13:22:56 -0500

    updates
    updates

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+//#include <"priorityQueue.h">
+#include <C:\Users\Emmanuel Jokotoye\Documents\Coding Files C++\lab09\priorityQueue.h>
+
+using namespace std;
+
+int main()
+{
+    priorityQueue firstQueue;
+
+    firstQueue.Insert();
+    firstQueue.Insert();
+    firstQueue.Insert();
+    firstQueue.Insert();
+    firstQueue.Insert();
+    firstQueue.Insert();
+    firstQueue.Insert();
+    firstQueue.Insert();
+    firstQueue.Insert();
+    firstQueue.Insert();
+
+
+    firstQueue.PrintQueue();
+
+    return 0;
+}
